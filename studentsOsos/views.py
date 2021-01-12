6fbd66dc5f4e4712993bb6ac889cbfa6c5838561
fohin <fohinek99@gmail.com>
2021-01-12 22:47:14 +0100

    Added new feature

--- studentsOsos/views.py
+++ studentsOsos/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'login.html')
+    return render(request, 'studentHome.html')
+
+
+def seePersonalData(request):
+    return render(request, 'studentPersonalData.html')
+
+
+def seeMessages(request):
+    return render(request, 'studentMessages.html')
+
+
+def seeGrades(request):
+    return render(request, 'studentGrades.html')
